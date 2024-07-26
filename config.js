ecbafeef586c378b84b40b289bd3b1c5040a6b5d
CodingwithLucy <143715585+CodingwithLucy@users.noreply.github.com>
2024-07-26 10:41:23 +0200

    names details changed and not used package deleted

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const DOMAIN = process.env.MG_DOMAIN;
-const APIKEY = process.env.MG_KEY;
+const API_KEY = process.env.MG_KEY;
 
-const mg = mailgun({ apiKey: APIKEY, domain: DOMAIN });
+const mg = mailgun({ apiKey: API_KEY, domain: DOMAIN });
 
 module.exports = mg;
